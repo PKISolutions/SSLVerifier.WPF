2531281c74d1a0d599b5f2ea73fede1124e484bb
Vadims Podans <vpodans@sysadmins.lv>
2020-07-05 20:31:54 +0300

    updated WPF library

---
+++ SSLVerifier.WPF/API/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SSLVerifier.API.ModelObjects;
+
+namespace SSLVerifier.API.Extensions {
+    static class IEnumerableExtensions {
+        public static void SaveAsCSV(this IEnumerable<ServerObject> list, String path) {
+            using (StreamWriter file = new StreamWriter(path)) {
+                file.WriteLine("Name,Port,Subject,NotBefore,NotAfter,Status");
+                foreach (ServerObject server in list) {
+                    file.WriteLine(server.ToString());
+                }
+            }
+        }
+    }
+}
