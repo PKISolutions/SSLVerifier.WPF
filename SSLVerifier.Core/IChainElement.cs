160e2864b83b6d5e24fac4f60f60a8d9aeb47954
Vadims Podāns <vpodans@sysadmins.lv>
2021-02-24 12:09:14 +0200

    clarified interface members and accessors. All accessors are read-only.

--- SSLVerifier.Core/IChainElement.cs
+++ SSLVerifier.Core/IChainElement.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Security.Cryptography.X509Certificates;
 using SSLVerifier.Core.Processor;
 
 namespace SSLVerifier.Core {
     public interface IChainElement {
-        String Name { get; set; }
-        X509Certificate2 Certificate { get; set; }
-        IChainElement Parent { get; set; }
+        String Name { get; }
+        X509Certificate2 Certificate { get; }
         String NativeErrorString { get; }
         String PropagatedErrorString { get; }
-        ObservableCollection<IChainElement> Child { get; }
-        Boolean IsRoot { get; set; }
-        Boolean HasErrors { get; set; }
-        Boolean HasWarnings { get; set; }
-        X509ChainStatusFlags2 NativeErrors { get; set; }
-        X509ChainStatusFlags2 PropagatedErrors { get; set; }
+        Boolean HasErrors { get; }
+        Boolean HasWarnings { get; }
+        X509ChainStatusFlags2 NativeErrors { get; }
+        X509ChainStatusFlags2 PropagatedErrors { get; }
     }
 }
--- SSLVerifier.Core/Models/ChainElement.cs
+++ SSLVerifier.Core/Models/ChainElement.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Security.Cryptography.X509Certificates;
 using SSLVerifier.Core.Processor;
 
 namespace SSLVerifier.Core.Models {
     class ChainElement : IChainElement {
-        public String Name { get; set; }
-        public X509Certificate2 Certificate { get; set; }
-        public IChainElement Parent { get; set; }
+        public String Name { get; internal set; }
+        public X509Certificate2 Certificate { get; internal set; }
         public String NativeErrorString => NativeErrors.ToString();
         public String PropagatedErrorString => PropagatedErrors.ToString();
-        public ObservableCollection<IChainElement> Child { get; }
-            = new ObservableCollection<IChainElement>();
-        public Boolean IsRoot { get; set; }
-        public Boolean HasErrors { get; set; }
-        public Boolean HasWarnings { get; set; }
-        public X509ChainStatusFlags2 NativeErrors { get; set; }
-        public X509ChainStatusFlags2 PropagatedErrors { get; set; }
+        public Boolean IsRoot { get; internal set; }
+        public Boolean HasErrors { get; internal set; }
+        public Boolean HasWarnings { get; internal set; }
+        public X509ChainStatusFlags2 NativeErrors { get; internal set; }
+        public X509ChainStatusFlags2 PropagatedErrors { get; internal set; }
     }
 }
