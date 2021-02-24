f15f922f76ea025ee1b90e95dfdd531118bed1b8
Vadims Podāns <vpodans@sysadmins.lv>
2021-02-24 11:53:06 +0200

    formatting

--- SSLVerifier.Core/Default/ServerLogWriter.cs
+++ SSLVerifier.Core/Default/ServerLogWriter.cs
@@ -4,8 +4,10 @@ using System.Text;
 namespace SSLVerifier.Core.Default {
     public class ServerLogWriter : IServerLogWriter {
         readonly StringBuilder _sb = new StringBuilder();
+
         public Int32 Progress { get; set; }
         public String Text => _sb.ToString();
+
         public void AppendLine(String text) {
             _sb.AppendLine(text);
         }
